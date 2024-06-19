17dfcba0f5ab7b91c6a37ecdb878762b2bc3fb1c
NIKNIK67 <1805niknik67@gmail.com>
2024-06-19 17:06:47 +0200

    Добавьте файлы проекта.

---
+++ WebApplication12/EFContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication12.Models;
+
+namespace WebApplication12
+{
+    public class EFContext : DbContext
+    {
+        public static DbSet<Item> Items { get; set; }
+        public EFContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.("test1");
+            optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message), LogLevel.Error);
+        }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Item>(entity =>
+            {
+                entity.HasKey(x => x.id);
+                entity.Property(x => x.name);
+                entity.Property(x => x.description);
+                entity.Property(x => x.isDeleted);
+
+            });
+            base.OnModelCreating(model);
+
+        }
+    }
+}
---
+++ WebApplication12/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication12.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool isDeleted { get; set; }
+
+    }
+}
